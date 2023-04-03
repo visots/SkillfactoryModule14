483075e2a7b289501b9ff4f0730d9f21b2926418
visot <visotskiy.alexey@gmail.com>
2023-04-03 20:53:44 +0700

    Добавьте файлы проекта.

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            List<string> sortedList = new();
+
+            foreach (string person in people)
+            {
+                if (person.StartsWith("А"))
+                    sortedList.Add(person);
+            }
+
+            sortedList.Sort();
+
+            foreach (string person in sortedList)
+                Console.WriteLine(person);
+        }
+    }
+}
