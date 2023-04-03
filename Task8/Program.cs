b2ff84c13f458e16f5fab3c8345048713b34914d
visot <visotskiy.alexey@gmail.com>
2023-04-03 21:33:56 +0700

    Задание 8

---
+++ Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>() { 1, "Сергей", "Андрей", 300 };
+            
+            //1
+            var sortedNames = from o in objects
+                              where o is string
+                              orderby o
+                              select o;
+
+            foreach (var name in sortedNames)
+                Console.WriteLine(name);
+
+            //2
+            foreach (var name in objects.Where(o => o is string).OrderBy(o => o))
+                Console.WriteLine(name);
+        }
+    }
+}
