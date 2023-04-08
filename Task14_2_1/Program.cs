38989f506a85b3cc1df1e7dc8789ed4da4bf5a74
visot <visotskiy.alexey@gmail.com>
2023-04-08 21:41:29 +0700

    Задание 14.2.1

---
+++ Task14_2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task14_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+
+            var orderedWords = words.OrderBy(x=>x.Length).Select(x=>
+            new
+            {
+                Word = x
+            });
+
+            foreach(var word in orderedWords)
+            {
+                Console.WriteLine(word.Word);
+            }
+        }
+    }
+}
