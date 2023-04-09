65c63e6373ecf4d2c313091261c3bac4e75f6e0d
visot <visotskiy.alexey@gmail.com>
2023-04-09 19:17:12 +0700

    Задание 14.2.10

---
+++ Task14_2_10/Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace Task14_2_10
+{
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ Task14_2_10/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task14_2_10
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            while (true)
+            {
+                var keyChar = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+
+                if (!Char.IsDigit(keyChar))
+                {
+                    Console.WriteLine("Ошибка ввода, введите число");
+                }
+                else
+                {
+                    IEnumerable<Contact> page = null;
+
+                    switch (keyChar)
+                    {
+                        case ('1'):
+                            page = phoneBook.Take(2);
+                            break;
+                        case ('2'):
+                            page = phoneBook.Skip(2).Take(2);
+                            break;
+                        case ('3'):
+                            page = phoneBook.Skip(4).Take(2);
+                            break;
+                    }
+
+                    if (page == null)
+                    {
+                        Console.WriteLine($"Ошибка ввода, страницы {keyChar} не существует");
+                        continue;
+                    }
+
+                    foreach (var contact in page)
+                        Console.WriteLine(contact.Name + " " + contact.LastName +" "+ contact.PhoneNumber +" "+contact.Email);
+                }
+            }
+
+        }
+    }
+}
